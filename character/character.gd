86a2964a08c6be1e425a9f7ba8663ec83a3cb469
smccourtb <shawnmccourtb@gmail.com>
2021-09-13 23:12:14 -0400

    very early stages of personality compatibilty checks for relationships and breeding.

--- character/character.gd
+++ character/character.gd
@@ -20,6 +20,7 @@ var eye_color_gene: Gene
 var height_gene: Gene
 var max_possible_height: int #inches
 
+var personality: Dictionary setget ,get_personality
 	
 func character_data() -> Dictionary:
 	return {"race": self.race, 
@@ -50,6 +51,46 @@ func determine_pronoun():
 				"possesive": "her",
 				"object": "her"}
 
+func get_personality():
+	var behavioural_traits = determine_behavioural_traits()
+	var likes = determine_likes()
+	var dislikes = determine_dislikes()
+	return {"behaviour":behavioural_traits,"likes":likes, "dislikes":dislikes}
 
+func determine_likes() -> Array:
+	var possible_traits = ['lazy', 'dumb', 'honest', 'intelligent', 'cleanfreak']
+	var likes = []
+	for _i in range(3):
+		likes.append(Util.choose(possible_traits))
+	return likes
+
+func determine_dislikes():
+	var possible_traits = ['lazy', 'dumb', 'honest', 'intelligent', 'cleanfreak']
+	var dislikes = []
+	for _i in range(3):
+		dislikes.append(Util.choose(possible_traits))
+	return dislikes
+
+func determine_behavioural_traits() -> Array:
+	var possible_traits = ['lazy', 'dumb', 'honest', 'intelligent', 'cleanfreak']
+	var traits = []
+	for _i in range(3):
+		traits.append(Util.choose(possible_traits))
+	return traits
+
+func determine_compatibility(other_pers):
+	# TODO: Add a family check
+
+	var behaviour = other_pers.behaviour
+	var compatibility = 0
+	for i in behaviour:
+		if i in personality.likes:
+			compatibility += 1
+		if i in personality.dislikes:
+			compatibility -= 1
+		if i in personality.behaviour:
+			compatibility += 2
+	return compatibility
+		
 
 
--- character/random_character.gd
+++ character/random_character.gd
@@ -4,7 +4,7 @@ var character_data
 var DWARF : = "res://character/dwarf.tres"
 
 var npc_choices: Array = [DWARF]
-
+var gen1 = []
 # Eye gene test variables
 var eye_genes = []
 
@@ -25,10 +25,35 @@ func generate_descendant() -> Descendant:
 	return character
 
 func _ready():
-	print("CharacterGenerator is ready")
-	randomize()
-	var a = OS.get_ticks_msec()
-#	for i in range(10000):
-	character_data = generate_descendant()
-	var b = OS.get_ticks_msec()
+# 	print("CharacterGenerator is ready")
+# 	randomize()
+# 	var a = OS.get_ticks_msec()
+# #	for i in range(10000):
+# 	character_data = generate_descendant()
+# 	var b = OS.get_ticks_msec()
 #	print("Generated 30,000 characters in " + str((b - a)/1000.00) + " seconds")
+	test_breeding()
+
+	
+func test_breeding():
+	for i in range(50):
+		var ancestor = generate_ancestor()
+		gen1.append(ancestor)
+	
+	# Now take 2 dwarfs and check compaitibility
+	var a = Util.choose(gen1)
+	var b = Util.choose(gen1)
+	var x = a.determine_compatibility(b.personality)
+	var y = b.determine_compatibility(a.personality)
+	print("CHARACTER1: ", x)
+	print("CHARACTER2: ", y)
+		
+
+# Generate a bunch of ancestors and put them in an array
+# Assuming they are in the same room and talking, check compatibility of two random dwarfs.
+# Check if they like eachother
+# Check if they are compatible to breed
+# If no to either save their relationship status and repeat from step 2
+# IF Yes, make a descendant and put them in generation 2 array
+# Dont remove them but if they have the honest trait and are above a certain compatiblity or have breeded them cancel it
+
