055783f88c3c1849aa2e43da32a4c2aab2709e89
smccourtb <shawnmccourtb@gmail.com>
2021-09-15 20:08:42 -0400

    refactor get_description function in trait resource.

--- trait.gd
+++ trait.gd
@@ -2,23 +2,25 @@ extends Resource
 class_name Trait
 
 var trait_name: String
-var trait_description: String
+var trait_descriptions: String
 
 func _init(t) -> void:
 	trait_name = t.name
+	trait_descriptions = t.description
+	
 
 func get_description(trait_value) -> String:
 	if trait_value >= 91:
-		return trait.descriptions[6]
+		return trait_descriptions[6]
 	elif trait_value >= 76:
-		return trait.descriptions[5]
+		return trait_descriptions[5]
 	elif trait_value >= 61:
-		return trait.descriptions[4]
+		return trait_descriptions[4]
 	elif trait_value > 40:
-		return trait.descriptions[3]
+		return trait_descriptions[3]
 	elif trait_value >= 25:
-		return trait.descriptions[2]
+		return trait_descriptions[2]
 	elif trait_value >= 10:
-		return trait.descriptions[1]
+		return trait_descriptions[1]
 	else:
-		return trait.descriptions[0]
+		return trait_descriptions[0]
