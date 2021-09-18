2f480205cb27623741dffe25b5bb0f7d8ad41258
smccourtb <shawnmccourtb@gmail.com>
2021-09-17 23:45:30 -0400

    add grid resource.

--- main.gd
+++ main.gd
@@ -1,23 +1,12 @@
 extends Node2D
 
 onready var character_generator = preload("res://character/CharacterGenerator.gd").new()
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for _i in range(2):
 		var node = load("res://character/Character.tscn").instance()
 		var x = character_generator.generate_ancestor()
-		
 		add_child(node)
 		node.data = x
 		node.speed = randi() % 25
-		print(x.name)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
