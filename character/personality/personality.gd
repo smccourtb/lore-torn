240e3803a2cc4f0385d7e6c7548f2f8c6b7c1faf
smccourtb <shawnmccourtb@gmail.com>
2021-09-23 23:42:36 -0400

    refactor personality generation

--- character/personality/personality.gd
+++ character/personality/personality.gd
@@ -1,45 +1,19 @@
 extends Resource
 class_name Personality
 # I will be restructing this following the OCEAN Model.
-var openness = PersonalityTrait.new(load("res://character/personality/neuroticism.tres"))
+var nueroticism = "neuroticism"
 # Concientiousness = PersonalityTrait.new()
 # Extraversion = PersonalityTrait.new()
 # Agreeableness = PersonalityTrait.new()
-# Nueroticism = PersonalityTrait.new()
-
-var facets: Dictionary
-var beliefs: Dictionary
-var goals: Dictionary
+# openness = PersonalityTrait.new()
+const TRAIT_STRINGS = ["neuroticism"]
+var traits: Dictionary
 
 func _init() -> void:
-	print(str(openness.get_total_value()) + "/600")
-	print(openness.facets.anxiety.get_current_value())
-	# facets = determine_traits()
-	# beliefs = determine_beliefs()
-	# goals = determine_goals()
+	generate_personality()
+	# print(str(nueroticism.get_total_value()) + "/600")
+	print(traits.neuroticism.facets.anxiety.get_current_value())
 
-# func determine_traits() -> Dictionary:
-# 	var _traits: = {}
-# 	var traits_list: Array = ['vanity', 'humor']
-# 	for i in traits_list:
-# 		var x = load("res://character/personality/facets/" + i + ".tres")
-# 		var t = Facet.new(x)
-# 		var value = Util.weighted_random(100, 5)
-# 		_traits[t.get_trait_name()] = {"value": value, 'description': t.get_description(value)}
-# 	return _traits
-
-# func determine_beliefs():
-# 	var _traits: = {}
-# 	var traits_list: Array = ['family']
-# 	for i in traits_list:
-# 		var x = load("res://character/personality/beliefs/" + i + ".tres")
-# 		var t = Belief.new(x)
-# 		var value = Util.weighted_random(100, 5)
-# 		_traits[t.get_trait_name()] = {"value": value, 'description': t.get_description(value)}
-# 	return _traits
-
-# func determine_goals():
-# 	var goal_list = ['love']
-# 	var resource = load("res://character/personality/goals/" + Util.choose(goal_list) + ".tres")
-# 	var trait = Goal.new(resource)
-# 	return  {'Goals': trait.get_trait_name()}
+func generate_personality():
+	for i in TRAIT_STRINGS:
+		traits[i] = PersonalityTrait.new(load("res://character/personality/" + i + ".tres"))
