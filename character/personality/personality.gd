59c0cb65e4a3a20b13a0b496baad3520ffa58659
smccourtb <shawnmccourtb@gmail.com>
2021-09-24 00:29:22 -0400

    fix facet generation again.. and incorporated all facets into personality generation

--- character/personality/personality.gd
+++ character/personality/personality.gd
@@ -1,19 +1,30 @@
 extends Resource
 class_name Personality
-# I will be restructing this following the OCEAN Model.
-var nueroticism = "neuroticism"
-# Concientiousness = PersonalityTrait.new()
-# Extraversion = PersonalityTrait.new()
-# Agreeableness = PersonalityTrait.new()
-# openness = PersonalityTrait.new()
-const TRAIT_STRINGS = ["neuroticism"]
+
+const openness = "openness"
+const concientiousness = "conscientiousness"
+const extraversion = "extroversion"
+const agreeableness = "agreeableness"
+const nueroticism = "neuroticism"
+
+const TRAIT_STRINGS = [openness, concientiousness, extraversion, agreeableness, nueroticism]
 var traits: Dictionary
 
 func _init() -> void:
 	generate_personality()
 	# print(str(nueroticism.get_total_value()) + "/600")
-	print(traits.neuroticism.facets.anxiety.get_current_value())
+	# print(traits.neuroticism.facets.anxiety.get_current_value())
 
 func generate_personality():
 	for i in TRAIT_STRINGS:
 		traits[i] = PersonalityTrait.new(load("res://character/personality/" + i + ".tres"))
+
+
+func get_trait_score(trait_name:String) -> int:
+	return traits[trait_name].get_total_value()
+
+func get_total_score() -> int:
+	var total: int = 0
+	for i in TRAIT_STRINGS:
+		total += get_trait_score(i)
+	return total
--- main.gd
+++ main.gd
@@ -5,9 +5,10 @@ onready var grid = preload("res://resource/grid/cell_grid.tres")
 onready var time = Time.new({'day':0, 'hour':0, 'minute':0, 'month':0, 'year':0}) # Use this to set the time of day when starting
 
 func _ready() -> void:
-	for _i in range(100):
+	for _i in range(1):
 		var node = load("res://character/Character.tscn").instance()
 		var x = character_generator.generate_ancestor()
+		print(x.personality.get_total_score())
 
 func _process(delta: float) -> void:
 	# time.advance(delta) # Start the clock
