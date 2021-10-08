701eaa80e6c4d6531a97501a5fd1d106a82941fa
smccourtb <shawnmccourtb@gmail.com>
2021-10-07 21:58:50 -0400

    remove GOAP

--- character/CharacterController.gd
+++ character/CharacterController.gd
@@ -66,7 +66,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed():
 		if event.get_scancode() == KEY_G:
 #			#This triggers the AI to start doing its thang
-			goap()
+			pass
 
 	
 func run_to(p):
@@ -158,97 +158,3 @@ func find_applicable_stockpile(what: String):
 				return false
 			return i.action(self, held)
 	return false
-			
-func get_goap_current_state() -> String:
-	var state = ""
-	# Check assigned jobs for what to do
-	for i in Global.jobs:
-		# if the job isnt assigned to the character
-		if data.assigned_jobs.find(i) == -1:
-			state += " !assigned_" + i
-		else:
-			state += " assigned_" + i
-	# Represents items on the ground
-	for i in ["wood"]:
-		if holds(i):
-			state += " has_"+i+" sees_"+i
-		else:
-			state += " !has_"+i
-			if Global.items.has(i) and Global.items[i].size() > 0:
-				if find_nearest_object(i, Global.items[i]).object != null:
-					state += " sees_"+i
-					
-
-	for o in ["tree"]:
-		if Global.resource_nodes.size() < 1:
-				state += " !sees_"+o
-		else:
-			state += " sees_"+o
-	
-	if Global.stockpiles.size() > 0:
-		state += " sees_stockpile"
-	else:
-		state += " !sees_stockpile"
-	state += " !hungry" #if (life < 75) else " !hungry"
-	state += " !tired" if (data.energy_level > 25) else " tired"
-	state += " !thirsty"
-	print("STATE: ", state)
-	return state
-
-func get_goap_current_goal():
-	var goal = ""
-	
-	# the goal is to plant trees then gather wood when there are enough trees
-	#if count_visible_objects("tree") < 10:
-	if 'chop' in data.assigned_jobs:
-		goal += " !sees_tree"
-	
-	goal += " wood_stored"
-	for i in goals['basic needs']:
-		goal += i
-	print("GOAL: ", goal)
-	return goal
-
-func goap():
-	# makes a plan based on current state and current goal
-	# loops through plan (list of actions) and performs each one
-	# if any of the actions returns false it breaks out and makes a new plan.
-# warning-ignore:unused_variable
-	var start_time = OS.get_unix_time()
-# warning-ignore:unused_variable
-	var count = 0
-	var action_planner = get_node("ActionPlanner")
-	if action_planner == null:
-		return
-	while true:
-		count += 1
-		#print("%d: Planning (%d)..." % [ OS.get_unix_time() - start_time, count ])
-		var plan : Array = action_planner.plan(get_goap_current_state(), get_goap_current_goal())
-		print('PLAN: ', plan)
-		# execute plan
-		for a in plan:
-			var error = false
-			# Actions are implemented as methods. meaning each actions needs a matching function to call
-			# - immediate actions return a boolean status
-			# - non immediate actions (that call yield) return a GDScriptFunctionState
-			if has_method(a):
-				#print("Calling action function "+a)
-				# calls the current action method
-				var status = call(a)
-				$Label.text = Util.titlefy(a)
-				# While action method is being performed wait for completion. expecting
-				# a return of true or false
-				while status is GDScriptFunctionState:
-					status = yield(status, "completed")
-				if typeof(status) != TYPE_BOOL:
-					print("Return value of "+a+" is not a boolean")
-					status = false
-				error = !status
-			else:
-				print("Cannot perform action "+a)
-				error = true
-			if error: break
-			timer.start()
-			yield(timer, "timeout")
-		timer.start()
-		yield(timer, "timeout")
