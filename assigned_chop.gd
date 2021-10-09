1f97bc63f51afd65b019ca6c10c9c4919ff7fdfa
smccourtb <shawnmccourtb@gmail.com>
2021-10-08 23:31:03 -0400

    fix bug where characters werent being unassigned jobs

--- assigned_chop.gd
+++ assigned_chop.gd
@@ -2,8 +2,9 @@ extends BTConditional
 
 
 func _pre_tick(agent: Node, blackboard: Blackboard) -> void:
+	verified = false	
 	for jobs in agent.data.assigned_jobs:
 		if jobs == "chop":
 			verified = true
-		else:
-			verified = false	
+	
+			
--- job_assigner_row.gd
+++ job_assigner_row.gd
@@ -21,6 +21,7 @@ func _on_Pressed(yo, boop, beep):
 	if boop.pressed:
 		beep.assign_new_job(Global.jobs[yo])
 	else:
+		print("SHOULD UNASSIGN")
 		beep.remove_assigned_job(Global.jobs[yo])
 
 func check_assigned(job: String, character):
